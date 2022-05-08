421c41c6a924d7327f3fdafe8d2339eb461d3f53
Luyn <xxxtrglyn@gmail.com>
2022-05-08 15:19:52 +0700

    *

--- src/modules/profile/index.jsx
+++ src/modules/profile/index.jsx
@@ -1,6 +1,3 @@
-import { Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
-import React, { useState } from "react";
-
 const Profile = () => {
   return;
 };
--- src/modules/upload/index.jsx
+++ src/modules/upload/index.jsx
@@ -1,8 +1,4 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
 import Page from "../components/page";
-import UserNav from "../components/usernav";
 
 const Upload = () => {
   return <Page></Page>;
