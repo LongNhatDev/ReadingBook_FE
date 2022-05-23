8f09057e67037a5c6a7cd759c44085241c94b64e
Luyn <xxxtrglyn@gmail.com>
2022-05-23 22:05:09 +0700

    fix conflict

--- src/modules/category/index.jsx
+++ src/modules/category/index.jsx
@@ -2,24 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import UserNav from "../components/usernav";
 import CateDetail from "./catedetail";
-import UserNav from "../components/usernav";
 
 const Category = () => {
   return (
-<<<<<<< HEAD
-    <React.Fragment>
-=======
     <>
->>>>>>> master-fe
       <UserNav />
       <Routes>
         <Route path="/:catename" element={<CateDetail />} />
       </Routes>
-<<<<<<< HEAD
-    </React.Fragment>
-=======
     </>
->>>>>>> master-fe
   );
 };
 
