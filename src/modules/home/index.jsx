c35dce6d0555a1ceeb9d2ddcf78405b14836fcd0
Long Nhat <tranlongnhat33333@gmail.com>
2022-06-03 01:09:31 +0700

    add base components

--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -11,7 +11,22 @@ const Home = () => {
       <UserNav />
       <Page>
         <Slider />
-        <Content></Content>
+        <Content>
+          <BoxRow>
+            <BoxCol>
+              <TitleH2>Weekly Book</TitleH2>
+              <Image
+                src="https://wallpaperaccess.com/full/2526660.jpg"
+                alt=""
+              />
+            </BoxCol>
+            <BoxCol>
+              <TitleH2>Recent Activities</TitleH2>
+
+            </BoxCol>
+            <div></div>
+          </BoxRow>
+        </Content>
         <Footer />
       </Page>
     </>
@@ -23,6 +38,43 @@ export default Home;
 const Content = styled.div`
   display: flex;
   flex-flow: column wrap;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 40px 0;
   width: 100%;
-  height: 800px;
+  min-height: 800px;
+`;
+
+const BoxCol = styled.div`
+  flex: 1;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  border: 1px solid black;
+`;
+
+const BoxRow = styled.div`
+  width: 80%;
+  height: 400px;
+  background-color: #ecebeb;
+  box-shadow: 2px 2px 6px grey;
+  margin-bottom: 40px;
+
+  display: flex;
+  flex-flow: row wrap;
+`;
+
+const Image = styled.img`
+  width: 80%;
+  height: 60%;
+  box-shadow: 2px 2px 3px 1px black;
+  border-radius: 5px;
+`;
+
+const TitleH2 = styled.h2`
+  margin: 20px 0;
+  line-height: 4rem;
+  border-bottom: 0.5px solid grey;
+  width: 80%;
 `;
